6643b385735971d7a57b5d71ee20be63c65170b4
Wagner <wagner.worst@gmail.com>
2023-04-25 15:10:03 -0300

    Project Created

---
+++ SalesWebModelViewControl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebModelViewControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

60f10c2b4a2db9ddc3924a4d148348e61110a124
Wagner <wagner.worst@gmail.com>
2023-05-04 18:42:47 -0300

    Simple Create Form

--- SalesWebModelViewControl/Controllers/SellersController.cs
+++ SalesWebModelViewControl/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebModelViewControl.Services;
+using SalesWebModelViewControl.Models;
 
 namespace SalesWebModelViewControl.Controllers
 {
@@ -15,6 +16,19 @@ namespace SalesWebModelViewControl.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) 
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
